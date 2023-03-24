296a9ea2ee1bc2a7f601eda805c7b0ef9038a9f8
Cales <86434489+calesj@users.noreply.github.com>
2023-03-24 13:36:28 -0300

    initial commit
    cars and motocycle system

---
+++ client/layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app dark>
+    <v-main>
+      <v-container>
+        <nuxt>
+        </nuxt>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+  data () {}
+  }
+</script>
---
+++ client/pages/index.vue
@@ -0,0 +1,76 @@
+<template>
+  <v-col class="shrink">
+    <v-app-bar app>
+    <v-btn :elevation="0"
+        @click="showCarros"
+      >
+        Carros
+      </v-btn>
+      <v-btn :elevation="0"
+        @click="showMotos"
+      >
+        Motos
+      </v-btn>
+    </v-app-bar>  
+      <v-scroll-x-transition>
+        <v-data-table
+        v-show="tableCarros"
+        :headers="headersCarros"
+        :items="itemsCarros"
+        hide-default-footer
+        ></v-data-table>
+      </v-scroll-x-transition>
+
+      <v-scroll-x-reverse-transition>
+        <v-data-table
+        v-show="tableMotos"
+        :headers="headersMotos"
+        :items="itemMotos"
+        hide-default-footer
+        ></v-data-table>
+      </v-scroll-x-reverse-transition>
+
+      
+  </v-col>
+</template>
+<script>
+  export default {
+    data () {
+      return {
+        headersCarros: [{
+          text: 'Carros',
+          value: 'car'
+        }],
+
+        headersMotos: [{
+          text: 'Motos'
+        }],
+        
+        itemsCarros: [{
+          car: 'Opala 166'
+      }],
+        itemsMotos: [],
+        tableCarros: true,
+        tableMotos: false
+     }
+    },
+
+    methods: {
+      async showCarros() {
+        this.tableMotos = false
+        await this.sleep(300)
+        this.tableCarros = true
+      },
+
+     async showMotos(){
+        this.tableCarros = false
+        await this.sleep(300)
+        this.tableMotos = true
+      },
+
+     sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+      }
+  }
+  }
+</script>
