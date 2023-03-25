c36a19f04de6dc01dd93aac86acf4c27bef87c63
Cales <86434489+calesj@users.noreply.github.com>
2023-03-25 18:47:53 -0300

    feat - refactor code infoCarComponent.vue

--- client/components/carros/infoCarComponent.vue
+++ client/components/carros/infoCarComponent.vue
@@ -2,17 +2,17 @@
     <div id="infoCarComponent">
         <v-dialog
             v-model="showDialog"
-            width="500">
-            <v-card>
+            width="500px">
+            <v-card class="pa-0 ma-0">
                 <v-card-title>Informações do Carro</v-card-title>
-                <v-card-text>Imagem:</v-card-text>
+                <v-card-text>Imagem: <v-img :src="carro.file_path"></v-img></v-card-text>
                 <v-card-text>Nome: {{carro.nome}}</v-card-text>
                 <v-card-text>Ano de Fabricação: {{carro.ano}}</v-card-text>
                 <v-card-actions>
 
                     <!--atualizar o valor da props showDialog para false
                     caso o usuario clique no botao e emitindo esse valor para o componente pai se atualizar com o novo valor dado-->
-                    <v-btn @click="fecharDialog()">
+                    <v-btn color="primary" @click="fecharDialog()">
                         Fechar
                     </v-btn>
                     </v-card-actions>
