d3103f423aa2f086c490e53f3a868763ad4d1eb6
Cales <86434489+calesj@users.noreply.github.com>
2023-03-25 10:58:36 -0300

    feat - add infoMotoComponent.vue, add componente in index

---
+++ client/components/motos/infoMotoComponent.vue
@@ -0,0 +1,39 @@
+<template>
+    <div id="infoCarComponent">
+        <v-dialog
+            v-model="showDialog"
+            width="500">
+            <v-card>
+                <v-card-title>Informações da Moto</v-card-title>
+                <v-card-text>Imagem: <v-img></v-img></v-card-text>
+                <v-card-text>Nome:</v-card-text>
+                <v-card-text>Ano de Fabricação:</v-card-text>
+                <v-card-actions>
+
+                    <!--atualizar o valor da props showDialog para false
+                    caso o usuario clique no botao e emitindo esse valor para o componente pai se atualizar com o novo valor dado-->
+                    <v-btn color="primary" @click="$emit('update:show-dialog', false)">
+                        Fechar
+                    </v-btn>
+                    </v-card-actions>
+            </v-card>
+        </v-dialog>
+    </div>
+</template>
+
+<script>
+export default{
+    name: 'infoMotoComponent',
+
+    //Definindo a props dinâmica que virá do componente pai 
+    props: {
+        showDialog: {
+            /*o valor que vier do componente pai, deve ser
+            booleano e o valor padrão é falso*/
+            type: Boolean,
+            default: false,
+        }
+    },
+}
+
+</script>
