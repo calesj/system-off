6b6cd20ca4e69a08c93c1f214c2e4763c778f8b0
Cales <86434489+calesj@users.noreply.github.com>
2023-03-25 20:40:29 -0300

    feat - add v-skeleton-loader

--- client/components/carros/infoCarComponent.vue
+++ client/components/carros/infoCarComponent.vue
@@ -1,35 +1,50 @@
 <template>
-    <div id="infoCarComponent">
-        <v-dialog
-            v-model="showDialog"
-            width="500px">
-            <v-card class="pa-0 ma-0">
-                <v-card-title>Informações do Carro</v-card-title>
-                <v-card-text>Imagem: <v-img :src="carro.file_path"></v-img></v-card-text>
-                <v-card-text>Nome: {{carro.nome}}</v-card-text>
-                <v-card-text>Ano de Fabricação: {{carro.ano}}</v-card-text>
-                <v-card-actions>
-
-                    <!--atualizar o valor da props showDialog para false
-                    caso o usuario clique no botao e emitindo esse valor para o componente pai se atualizar com o novo valor dado-->
-                    <v-btn color="primary" @click="fecharDialog()">
-                        Fechar
-                    </v-btn>
-                    </v-card-actions>
-            </v-card>
-        </v-dialog>
-    </div>
+  <div id="infoCarComponent">
+    <v-dialog
+      v-model="showDialog"
+      width="500px">
+      <v-card class="mx-auto">
+        <div v-if="loading">
+          <v-skeleton-loader
+            class="auto"
+            type="list-item-avatar-three-line, image, article"
+          ></v-skeleton-loader>
+        </div>
+        <div v-show="!loading">
+          <v-card-title>Informações do Carro</v-card-title>
+          <v-card-text><b>Imagem:</b>
+            <v-img :src="carro.file_path"></v-img>
+          </v-card-text>
+          <v-card-text><b>Nome:</b> {{ carro.nome }}</v-card-text>
+          <v-card-text><b>Ano de Fabricação:</b> {{ carro.ano }}</v-card-text>
+        </div>
+        <v-card-actions>
+          <!--atualizar o valor da props showDialog para false
+          caso o usuario clique no botao e emitindo esse valor para o componente pai se atualizar com o novo valor dado-->
+          <v-btn color="primary" @click="fecharDialog()">
+            Fechar
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'infoCarComponent',
 
+  data() {
+    return {
+      loading: true
+    }
+  },
+
   //Definindo a props dinâmica que virá do componente pai
   props: {
     showDialog: {
       /*o valor que vier do componente pai, deve ser
-            booleano e o valor padrão é falso*/
+        booleano e o valor padrão é falso*/
       type: Boolean,
       default: false,
     },
@@ -39,6 +54,13 @@ export default {
       required: true
     }
   },
+
+  mounted() {
+    setTimeout(() => {
+      this.loading = false
+    }, 1000)
+  },
+
   methods: {
     fecharDialog() {
       this.$emit('fechar-dialog')
