b207be2a9d3294935969b1b893a825b4db862457
Cales <86434489+calesj@users.noreply.github.com>
2023-03-26 12:04:58 -0300

    feat - add directory

---
+++ client/components/motos/motoForm.vue
@@ -0,0 +1,47 @@
+<template>
+<div id="motoForm">
+  <v-dialog max-width="800px"
+  v-model="dialog">
+    <v-card height="700px">
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn @click="closeDialog()" icon>
+          <v-icon>mdi-close</v-icon>
+        </v-btn>
+      </v-card-actions>
+      <v-card-subtitle>Cadastrar Moto</v-card-subtitle>
+    </v-card>
+  </v-dialog>
+</div>
+</template>
+
+<script>
+
+import props from "vuetify/src/components/VCalendar/util/props";
+export default {
+  name: 'motoForm',
+
+  data () {
+    return {}
+  },
+  // utilizamos props, para o componente pai poder manipular o valor dessa propriedade por fora
+  props: {
+
+    // dialog é a propriedade que representa o v-model do nosso componente, utilizamos props, para o componente pai
+    // poder manipular o valor dessa propriedade por fora
+    dialog: {
+      type: Boolean,
+      required: true
+    }
+  },
+
+  methods: {
+
+    // evento será emitido para o componente pai, caso usuario clique no botão fechar, componente pai irá saber
+    closeDialog() {
+      this.$emit('close-dialog')
+    }
+  }
+}
+</script>
+
